90a46341ffd7b538c5a4e5151621086dbe46e382
andreakbarreiro <abarreiro@smu.edu>
2022-10-02 10:50:38 -0500

    update test_plot_UV
    update test_plot_UV to use compAllWallBdy

--- code/test_plot_UV.m
+++ code/test_plot_UV.m
@@ -8,31 +8,22 @@ addpath('util_geom');
 read_polyMesh_flag = 1;
 read_uv_flag       = 1;
 if (read_polyMesh_flag)
-    caseDir = '../caseDirs/Elad_Test_Case/';
+    caseDir = '../caseDirs/Elad_Test_Case_10s/';
 
     % Boundary file name
     fname   = 'constant/polymesh/boundary';
 
-    %[fp fstats]=facestats([caseDir fname]);
+    [fp fstats]=facestats([caseDir fname]);
     [R FACE S lmax] = read_polyMesh(caseDir);
 end
 if (read_uv_flag)
     uvfile = 'uv2.obj';
     [r face r2R]=read_uv([caseDir uvfile]);
 end
-timestep    = 1.5;
+timestep    = 8.5;
 wssfname    = sprintf('%s%g/wallShearStress',caseDir,timestep);
 
 w = read_wss_mag_OF_face(wssfname);
-
-% Get boundary
-% # of wall faces per region
-wallFaces = fp(:,2).*fp(:,3);
-wallFaces(wallFaces==0)=[];
-
-startI = [0;cumsum(wallFaces)]+1;
-startI = startI(1:end-1); endI = cumsum(wallFaces);
-
 
 % w should be defined ON THE SAME FACES
 % i.e. there is a 1-1 mapping between faces in the 3D model
@@ -44,13 +35,15 @@ patch(U_patch,V_patch,w','LineStyle','None');
 axis equal;axis tight;
 colorbar;
 
+
+% Obtain boundary
+lbAll = compAllWallBdy(fp,r,face);
+
 hold on;
-lbAll = cell(length(wallFaces),1);
-for j=1:length(wallFaces)
-    [lb,Ub,Vb,Zb]=findb([r zeros(length(r),1)],face(startI(j):endI(j),:));
-    temp = []; temp.lb = lb; temp.Ub=Ub; temp.Vb = Vb;
-    lbAll{j}=temp;
-    %line(Ub,Vb,Zb,'Color','r','LineWidth',2);
+colorstr = {'r';'y';'g';'c'};
+for j=1:length(lbAll)
+    temp=lbAll{j};
+    line(temp.Ub,temp.Vb,zeros(size(temp.Ub)),'Color',colorstr{j},'LineWidth',2);
 end
 
 set(gca,'FontSize',16);
